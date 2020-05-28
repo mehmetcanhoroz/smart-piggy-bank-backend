6088c3a68b526f440e8e25a67cc603086529197f
Mehmet CANHOROZ <mehmetcanhoroz@gmail.com>
2020-05-28 09:57:46 -0500

    Try to deploy project on digitalocean

--- app/Providers/DataShareServiceProvider.php
+++ app/Providers/DataShareServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Coin;
 use App\Models\Transaction;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,20 +29,24 @@ class DataShareServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $userCount = User::all()->count();
-        $transactions = Transaction::all();
-        //$transactionCount = Transaction::all()->count();
-        $transactionCount = $transactions->count();
-        //$calendarCount = Transaction::all()->where('created_at', Carbon::today())->count();
-        //dd(Carbon::today());
-        $calendarCount = $transactions->whereBetween('created_at', [Carbon::today(), Carbon::today()->addDay()])->count();
-        $totalSaving = Coin::all()->sum('value');
-        //dd($totalSaving);
+        try {
+            $userCount = User::all()->count();
+            $transactions = Transaction::all();
+            //$transactionCount = Transaction::all()->count();
+            $transactionCount = $transactions->count();
+            //$calendarCount = Transaction::all()->where('created_at', Carbon::today())->count();
+            //dd(Carbon::today());
+            $calendarCount = $transactions->whereBetween('created_at', [Carbon::today(), Carbon::today()->addDay()])->count();
+            $totalSaving = Coin::all()->sum('value');
+            //dd($totalSaving);
 
-        // Sharing is caring
-        View::share('userCount', $userCount);
-        View::share('transactionCount', $transactionCount);
-        View::share('calendarCount', $calendarCount);
-        View::share('totalSaving', $totalSaving);
+            // Sharing is caring
+            View::share('userCount', $userCount);
+            View::share('transactionCount', $transactionCount);
+            View::share('calendarCount', $calendarCount);
+            View::share('totalSaving', $totalSaving);
+        } catch (Exception $exception) {
+            Log::error('DataShareServiceProvider Error! Probably first installation.', $exception);
+        }
     }
 }
