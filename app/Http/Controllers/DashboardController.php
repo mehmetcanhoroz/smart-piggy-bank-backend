af720fe6236ecff67e08a21a985371995e247362
Mehmet CANHOROZ <mehmetcanhoroz@gmail.com>
2020-05-28 19:14:34 -0500

    Added Api methods

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coin;
+use App\Models\Transaction;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        if ($request->wantsJson()) {
+            $userCount = User::all()->count();
+            $transactions = Transaction::all();
+            //$transactionCount = Transaction::all()->count();
+            $transactionCount = $transactions->count();
+            //$calendarCount = Transaction::all()->where('created_at', Carbon::today())->count();
+            //dd(Carbon::today());
+            $calendarCount = $transactions->whereBetween('created_at', [Carbon::today(), Carbon::today()->addDay()])->count();
+            $totalSaving = Coin::all()->sum('value');
+            //dd($totalSaving);
+
+            // Sharing is caring
+//            View::share('userCount', $userCount);
+//            View::share('transactionCount', $transactionCount);
+//            View::share('calendarCount', $calendarCount);
+//            View::share('totalSaving', $totalSaving);
+        }
         return view('pages.index');
     }
 }
